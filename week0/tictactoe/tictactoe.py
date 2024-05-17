ef9eb79fdf594da6b9cfdb33bceeab3445390a41
whitealpa <132802748+whitealpa@users.noreply.github.com>
2024-05-17 16:02:42 +0700

    Finished week0

--- week0/tictactoe/tictactoe.py
+++ week0/tictactoe/tictactoe.py
@@ -22,15 +22,6 @@ def player(board):
     """
     Returns player who has the next turn on a board.
     """
-
-    """
-    The player function should take a board state as input, 
-    and return which player’s turn it is (either X or O).
-    
-    In the initial game state, X gets the first move. Subsequently, the player alternates with each additional move.    
-    
-    Any return value is acceptable if a terminal board is provided as input (i.e., the game is already over).
-    """
     
     x_moves = sum(row.count(X) for row in board)
     o_moves = sum(row.count(O) for row in board)
@@ -43,24 +34,12 @@ def actions(board):
     Returns set of all possible actions (i, j) available on the board.
     """
     
-    """
-    The actions function should return a set of all of the possible actions that can be taken on a given board.
-    
-    Each action should be represented as a tuple (i, j) 
-    where i corresponds to the row of the move (0, 1, or 2) 
-    and j corresponds to which cell in the row corresponds to the move (also 0, 1, or 2).
-    
-    Possible moves are any cells on the board that do not already have an X or an O in them.
-    
-    Any return value is acceptable if a terminal board is provided as input.
-    """
-    
     possible_moves = set()
     
-    for i, row in enumerate(board):
-        for j, action in enumerate(row):
-            if action == EMPTY:
-                possible_moves.add((i, j))
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                possible_moves.add((i, j))  
     
     return possible_moves
 
@@ -70,35 +49,21 @@ def result(board, action):
     Returns the board that results from making move (i, j) on the board.
     """
     
-    """
-    The result function takes a board and an action as input, and should return a new board state, without modifying the original board.
-    
-    If action is not a valid action for the board, your program should raise an exception.
-    
-    The returned board state should be the board that would result from taking the original input board, 
-    and letting the player whose turn it is make their move at the cell indicated by the input action.
-    
-    Importantly, the original board should be left unmodified: 
-    since Minimax will ultimately require considering many different board states during its computation. 
-    This means that simply updating a cell in board itself is not a correct implementation of the result function. 
-    You’ll likely want to make a deep copy of the board first before making any changes.
-    """
-    
-    # Check for current player
-    current_player = player(board)
     i, j = action
+    
+    # Check for out-of-bound moves
+    if not (0 <= i < 3 and 0 <= j < 3):
+        raise Exception("Out-of-bound moves")
     
     # Deep copy the board
     new_board = [row[:] for row in board] 
-    
+
     # Check if move is valid
     if new_board[i][j] != EMPTY:
         raise Exception("Invalid move.")
     else:
-        new_board[i][j] = current_player
-    
-    return new_board
-    
+        new_board[i][j] = player(board)
+        return new_board
 
 
 def winner(board):
@@ -106,37 +71,20 @@ def winner(board):
     Returns the winner of the game, if there is one.
     """
     
-    """
-    The winner function should accept a board as input, and return the winner of the board if there is one.
-        
-    If the X player has won the game, your function should return X. 
-    If the O player has won the game, your function should return O.
-    
-    One can win the game with three of their moves in a row horizontally, vertically, or diagonally.
-    
-    You may assume that there will be at most one winner 
-    (that is, no board will ever have both players with three-in-a-row, 
-    since that would be an invalid board state).
-    
-    If there is no winner of the game (either because the game is in progress, or because it ended in a tie), 
-    the function should return None.
-    """
-    
     # Check for winner (3 in a row or column)
     for i in range(3):
         
         # Check row
-        if board[i][0] == board[i][1] == board[i][2]:
-            return board[i][0]
-        
+        if board[i][0] == board[i][1] == board[i][2] and board[i][0] != None:
+            return board[i][0] 
         # Check column
-        elif board[0][i] == board[1][i] == board[2][i]:
+        if board[0][i] == board[1][i] == board[2][i] and board[0][i] != None:
             return board[0][i]
         
     # Check diagonal
-    if board[0][0] == board[1][1] == board[2][2]:
+    if board[0][0] == board[1][1] == board[2][2] and board[0][0] != None:
         return board[0][0]
-    elif board[0][2] == board[1][1] == board[2][0]:
+    elif board[0][2] == board[1][1] == board[2][0] and board[0][2]:
         return board[0][2]
         
     # If no winner, return None
@@ -147,20 +95,9 @@ def terminal(board):
     """
     Returns True if game is over, False otherwise.
     """
-    
-    """
-    The terminal function should accept a board as input, 
-    and return a boolean value indicating whether the game is over.
 
-    If the game is over, either because someone has won the game 
-    or because all cells have been filled without anyone winning, 
-    the function should return True.
-    
-    Otherwise, the function should return False if the game is still in progress.
-    """
-    
     # If there's a winner, game is over
-    if winner(board) != None:
+    if winner(board):
         return True
     
     # If there is still an empty cell, game is not over
@@ -178,16 +115,6 @@ def utility(board):
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
     
-    """
-    The utility function should accept a terminal board as input and output the utility of the board.
-    
-    If X has won the game, the utility is 1. 
-    If O has won the game, the utility is -1. 
-    If the game has ended in a tie, the utility is 0.
-    
-    You may assume utility will only be called on a board if terminal(board) is True.
-    """
-    
     if winner(board) == X:
         return 1
     elif winner(board) == O:
@@ -201,25 +128,54 @@ def minimax(board):
     Returns the optimal action for the current player on the board.
     """
     
-    """
-    The minimax function should take a board as input, 
-    and return the optimal move for the player to move on that board.
-    
-    The move returned should be the optimal action (i, j) that is one of the allowable actions on the board. 
-    If multiple moves are equally optimal, any of those moves is acceptable.
-    
-    If the board is a terminal board, the minimax function should return None.
-    """
-    
     if terminal(board):
         return None
-    
 
-def main():
-    board = initial_state()
-    
-    print(winner(board))
-    
+    if player(board) == X:
+        beta = float("-inf")
+        for action in actions(board):
+            value = min_value(result(board, action))
+            
+            # Condition to find the highest value possible from all possible actions
+            if value == 1:
+                return action
+            elif value > beta:
+                beta = value
+                move = action
+    else:
+        alpha = float("inf")
+        for action in actions(board):
+            value = max_value(result(board, action))
 
-if __name__ == "__main__":
-    main()
+            # Condition to find the lowest value possible from all possible actions
+            if value == -1:
+                return action
+            elif value < alpha:
+                alpha = value
+                move = action
+                
+    return move
+
+
+def max_value(board):
+
+    value = float("-inf")
+    
+    if terminal(board):
+        return utility(board)
+
+    for action in actions(board):
+        value = max(value, min_value(result(board, action)))
+    return value
+
+
+def min_value(board):
+    
+    value = float("inf")
+    
+    if terminal(board):
+        return utility(board)
+
+    for action in actions(board):
+        value = min(value, max_value(result(board, action)))
+    return value
